b59eeaeed55447085434cc2a5ddfe98f6c1a1925
Srishanth <talakantisrishanth1629@gmail.com>
2026-02-10 17:46:42 +0530

    Fixed mobile layout bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,22 +10,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider afterSignOutUrl="/">    
-    <html lang="en">
-      
-      <body className="h-screen flex flex-col">
-        <SignedIn>
-        <Navbar />
-        <div className="flex-1 min-h-0 overflow-auto">
-          <Providers>
-            {children}  
-            </Providers></div>
-     </SignedIn>
-     <SignedOut>
-      <HeroSection/>
-      </SignedOut> 
-      </body>
-      
-    </html></ClerkProvider>
+    <ClerkProvider afterSignOutUrl="/">
+      <html lang="en">
+        <body className="min-h-screen flex flex-col overflow-x-hidden">
+          <SignedIn>
+            <Navbar />
+            <div className="flex-1 min-h-0 overflow-auto overflow-x-hidden">
+              <Providers>{children}</Providers>
+            </div>
+          </SignedIn>
+
+          <SignedOut>
+            <HeroSection />
+          </SignedOut>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
+
